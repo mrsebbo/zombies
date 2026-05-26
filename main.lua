dddc940d930c8859dac42667ad5c7ddc9979b90e
Sebastian Banker <mrsebbo@gmail.com>
2026-05-26 16:54:10 -0400

    ooh, I pasted in a code!

---
+++ main.lua
@@ -0,0 +1,7 @@
+--[[
+in keeping with a more iterative approach, this version of Main will start out 
+including much of the functionality of an anticipated future PlayState. For now, 
+I'm skipping art and animation to focus on functionality.
+]]
+
+love.graphics.setDefaultFilter('nearest', 'nearest')
